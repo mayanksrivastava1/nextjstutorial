6c8629f67cbc6ed53c1a1abbbdbb9a1017c3318f
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2023-10-16 20:33:18 +0530

    router done

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 
 export default function page() {
+    const [data, setData] = useState([])
     useEffect( async () => {
       let data = await fetch('')
       data = await data.json()
